efbfa18a29a716a5d1ded3a67b2d3c14055414ab
Francisco Santana <bragasan34@gmail.com>
2025-03-27 16:07:18 -0400

    locale

--- BragaMusic/settings.py
+++ BragaMusic/settings.py
@@ -84,7 +84,23 @@ else:
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
+        
     }
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
 
 AUTH_USER_MODEL = "instrumento.Cliente"
 
