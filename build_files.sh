4a08f7d63934cfa16e7834b2c81bec400bcf4e36
Francisco Santana <bragasan34@gmail.com>
2025-03-27 15:10:02 -0400

    ajuste de bash

--- build_files.sh
+++ build_files.sh
@@ -4,5 +4,6 @@
 python3 -m pip install -r requirements.txt
 
 # Executa as migrações e coleta os arquivos estáticos
-python3 manage.py migrate
+
 python3 manage.py collectstatic --noinput
+python3 manage.py migrate
