64ddcce505c2980fd74e8c751c29f5246e63ee15
Francisco Santana <bragasan34@gmail.com>
2025-03-27 14:44:31 -0400

    ajuste de bash

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# Instala as dependências do projeto Django a partir do arquivo requirements.txt
 python3 -m pip install -r requirements.txt
-python3 manage.py migrate
-python3 manage.py collectstatic --noinput
+
+# Verifica se a variável de ambiente PROD está vazia ou não definida
+if [ -z "$PROD" ]; then
+
+    # Se PROD não estiver definida (ambiente local), executa as migrações e coleta os arquivos estáticos
+    python3 manage.py migrate
+    python3 manage.py collectstatic --noinput
+
+fi # Fim do bloco if
