04e72b417b2dd589d8fcb69c7ae4fc10975df158
Francisco Santana <bragasan34@gmail.com>
2025-03-27 15:03:02 -0400

    ajuste no staticfiles

--- BragaMusic/settings.py
+++ BragaMusic/settings.py
@@ -102,7 +102,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'static')
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 
 
