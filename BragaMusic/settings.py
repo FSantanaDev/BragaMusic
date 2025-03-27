de76c983b6a103dffc9ddb58a4a6e3737263c0bd
Francisco Santana <bragasan34@gmail.com>
2025-03-27 15:08:01 -0400

    ajuste de ROOT

--- BragaMusic/settings.py
+++ BragaMusic/settings.py
@@ -95,7 +95,7 @@ USE_TZ = True
 
 
 
-STATIC_URL = 'static/'
+SSTATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
@@ -103,8 +103,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build')
 
 
 LOGGING = {
--- build_files.sh
+++ build_files.sh
@@ -3,11 +3,6 @@
 # Instala as dependências do projeto Django a partir do arquivo requirements.txt
 python3 -m pip install -r requirements.txt
 
-# Verifica se a variável de ambiente PROD está vazia ou não definida
-if [ -z "$PROD" ]; then
-
-    # Se PROD não estiver definida (ambiente local), executa as migrações e coleta os arquivos estáticos
-    python3 manage.py migrate
-    python3 manage.py collectstatic --noinput
-
-fi # Fim do bloco if
+# Executa as migrações e coleta os arquivos estáticos
+python3 manage.py migrate
+python3 manage.py collectstatic --noinput
